604f887a0c36065c7e787e55e07da420aa4de87c
alexandre katsarava <139011719+AlexGOAstudent@users.noreply.github.com>
2023-09-11 13:08:00 +0400

    Add files via upload

---
+++ homework.py
@@ -0,0 +1,8 @@
+name = "alexandre"
+last_name = "katsarava"
+age = "12"
+live_in = "bagebi"
+height = "1m 56sm"
+
+
+print("ჩემი სახელია " + name + " ჩემი გვარია " + last_name + " მე ვარ " + age + " მე ვცხოვრობ " + live_in + " ჩემი სიმაღლეა " + height)
